cc556d1458ae84e719f1ef18dd79dcad7bb19e5c
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-08-07 09:29:03 +0800

    Add Project and TodoItem classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./style.css"
+
+class Project {
+    constructor(title) {
+        this.title = title;
+    }
+
+    get title() {
+        return this.title;
+    }
+}
+
+class TodoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    get title() {
+        return this.title;
+    }
+
+    get description() {
+        return this.description;
+    }
+
+    get dueDate() {
+        return this.dueDate;
+    }
+
+    get priority() {
+        return this.priority;
+    }
+}
