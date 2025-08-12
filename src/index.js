6331aad2d02d194f2b904e63222737767fb959e6
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-08-12 11:28:15 +0800

    Add todo IIFE to manage projects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import "./style.css"
 
-const projectsList = [];
+const todo = (() => {
+    let projectsList = [];
+    let currentProject = projectsList[0];
+
+    return {
+        addProject(project) {
+            projectsList.push(project);
+        },
+        removeProject(projectId) {
+            projectsList = projectsList.filter(project => project.id !== projectId);
+        },
+        addProjectTask(task) {
+            currentProject.addTask(task);
+        }
+    }
+})();
 
 class Project {
     constructor(title) {
         this._title = title;
         this._todoList = [];
+        this._id = crypto.randomUUID()            
     }
 
     get title() {
@@ -16,21 +32,26 @@ class Project {
         this._title = title;
     }
 
-    addTodo(todo) {
-        this._todoList.push(todo);
+    addTask(task) {
+        this._todoList.push(task);
     }
 
     get todoList() {
         return this._todoList;
     }
+
+    get id() {
+        return this._id;
+    }
 }
 
-class TodoItem {
+class Task {
     constructor(title, description, dueDate, priority) {
         this._title = title;
         this._description = description;
         this._dueDate = dueDate;
         this._priority = priority;
+        this._id = crypto.randomUUID()            
     }
 
     get title() {
@@ -47,6 +68,10 @@ class TodoItem {
 
     get priority() {
         return this._priority;
+    }
+
+    get id() {
+        return this._id;
     }
 
     set title(title) {
@@ -87,14 +112,15 @@ function setupDialog(openBtnSelector, dialogId, onSubmit) {
 // Project dialog
 setupDialog(".project-btn", "create-project", (dialog) => {
     const title = dialog.querySelector("#project-title").value;
-    projectsList.push(new Project(title));
+    todo.addProject(new Project(title));
 });
 
 // Task dialog
 setupDialog(".task-btn", "create-task", (dialog) => {
     const title = dialog.querySelector("#task-title").value;
     const desc = dialog.querySelector("#desc").value;
-    const dueDate = dialog.querySelector("#due-date").value;
+    const dueDate = new Date(dialog.querySelector("#due-date").value);
     const priority = dialog.querySelector('input[name="priority"]:checked').value;
+    todo.addProjectTask(new Task(title, desc, dueDate, priority));
 });
 
