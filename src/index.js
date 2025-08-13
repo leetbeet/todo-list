037e0dd89008cf032625cd726f3ef624cdc38907
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-08-13 12:21:22 +0800

    Add feature to load tasks when project is clicked

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import "./style.css"
 
-const todo = (() => {
-    let projectsList = [];
-    let currentProject = projectsList[0];
-
-    return {
-        addProject(project) {
-            projectsList.push(project);
-        },
-        removeProject(projectId) {
-            projectsList = projectsList.filter(project => project.id !== projectId);
-        },
-        addProjectTask(task) {
-            currentProject.addTask(task);
-        }
-    }
-})();
-
 class Project {
     constructor(title) {
         this._title = title;
         this._todoList = [];
-        this._id = crypto.randomUUID()            
+        this._id = crypto.randomUUID();            
     }
 
     get title() {
@@ -51,7 +34,7 @@ class Task {
         this._description = description;
         this._dueDate = dueDate;
         this._priority = priority;
-        this._id = crypto.randomUUID()            
+        this._id = crypto.randomUUID();            
     }
 
     get title() {
@@ -91,6 +74,61 @@ class Task {
     }
 }
 
+const todo = (() => {
+    let projectsList = [new Project("Today")];
+    const projectToday = document.querySelector(".project-btn");
+    projectToday.id = projectsList[0]._id;
+    projectToday.addEventListener("click", () => projectEventListener(projectsList[0]._id));    
+    let currentProject = projectsList[0];
+
+    return {
+        addProject(project) {
+            projectsList.push(project);
+            return project._id;
+        },
+        removeProject(projectId) {
+            projectsList = projectsList.filter(project => project.id !== projectId);
+        },
+        addProjectTask(task) {
+            currentProject.addTask(task);
+        },
+        updateCurrentProject(projectId) {
+            currentProject = projectsList.find(project => project.id === projectId);
+        },
+        getCurrentProject() {
+            return currentProject;
+        }
+    };
+})();
+
+const loadTasks = () => {
+    const currentProject = todo.getCurrentProject();
+    const tasksTab = document.querySelector(".tasks-tab");
+    tasksTab.innerHTML = "";
+    const todoList = currentProject._todoList;
+
+    for (const task of todoList) {
+        const taskItem = document.createElement("div");
+        taskItem.className = "task";
+        taskItem.id = task._id;
+
+        const taskTitle = document.createElement("div");
+        taskTitle.className = "task-title";
+        taskTitle.textContent = task._title;
+        const taskDueDate = document.createElement("div");
+        taskDueDate.className = "task-due-date";
+        taskDueDate.textContent = task._dueDate;
+
+        taskItem.append(taskTitle, taskDueDate);
+        tasksTab.append(taskItem);
+    }
+}
+
+function projectEventListener(projectId) {
+    todo.updateCurrentProject(projectId);
+    loadTasks();
+}
+
 function setupDialog(openBtnSelector, dialogId, onSubmit) {
     document.querySelector(openBtnSelector).addEventListener("click", () => {
         const dialog = document.getElementById(dialogId);
@@ -101,6 +139,7 @@ function setupDialog(openBtnSelector, dialogId, onSubmit) {
         }, { once: true });
 
         const form = dialog.querySelector("form");
+        form.reset();
         form.addEventListener("submit", function(e) {
             e.preventDefault();
             onSubmit(dialog);
@@ -110,9 +149,17 @@ function setupDialog(openBtnSelector, dialogId, onSubmit) {
 }
 
 // Project dialog
-setupDialog(".project-btn", "create-project", (dialog) => {
+setupDialog(".add-project-btn", "create-project", (dialog) => {
     const title = dialog.querySelector("#project-title").value;
-    todo.addProject(new Project(title));
+    const idText = todo.addProject(new Project(title));
+
+    const projectsTab = document.querySelector(".projects");
+    const project = document.createElement("button");
+    project.id = idText;
+    project.className = "project-btn";
+    project.textContent = title;
+    projectsTab.appendChild(project);
+    project.addEventListener("click", () => projectEventListener(project.id));
 });
 
 // Task dialog
@@ -122,5 +169,5 @@ setupDialog(".task-btn", "create-task", (dialog) => {
     const dueDate = new Date(dialog.querySelector("#due-date").value);
     const priority = dialog.querySelector('input[name="priority"]:checked').value;
     todo.addProjectTask(new Task(title, desc, dueDate, priority));
-});
-
+    loadTasks();
+}); 
